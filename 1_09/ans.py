dc56f5b421bdfe625f3d98fb3994449463974adf
jungyu0413 <jungyu0413@gmail.com>
2023-05-08 10:55:54 +0900

    day9

--- 1_09/ans.py
+++ 1_09/ans.py
@@ -6,16 +6,16 @@ for i in range(n):
     tmp = input().split()
     tmp.sort()
     a,b,c = map(int, tmp)
-    if a==b and b==c:
+    if a == b and b == c:
         money = 10000+a*1000
-    elif a==b or a==c:
+    elif a == b or a == c:
         money = 1000+a*100
-    elif b==c:
+    elif b == c:
         money = 1000+b*100
     else:
         money = c*100
-    if money>res:
-        res=money
+    if money > res:
+        res = money
 
 print(res)
 
