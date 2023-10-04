bf0e26db3d21b5b9085f40bb3f547ecc395e48b7
jungyu0413 <jungyu0413@gmail.com>
2023-10-05 07:49:33 +0900

    day14

--- 1_14/ans.py
+++ 1_14/ans.py
@@ -6,13 +6,22 @@ a1 = list(map(int, input().split()))
 n2 = int(input())
 a2 = list(map(int, input().split()))
 
-total = a1 + a2
-re_total = []
+total_a = []
 
-min = 999999
-for i in total:
-    if i < min:
-        min = i
+p1=p2=0
+
+while p1<n1 and p2<n2:
+    if a1[p1] <= a2[p2]:
+        total_a.append(a1[p1])
+        p1 += 1
+    else:
+        total_a.append(a2[p2])
+        p2 += 1
+
+if p1 < n1:
+    total_a = total_a + a1[p1:]
+if p2 < n2:
+    total_a = total_a + a2[p2:]
 
 
-
+print(total_a)
